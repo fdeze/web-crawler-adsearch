52ea867bc240070d63557628affa474dbe15af45
fab <fab@LAPTOP-TG7U2CSN>
2019-04-07 18:19:44 +0200

    d

--- src/test/java/fr/fabien/webcrawler/WebCrawlerApplicationTests.java
+++ src/test/java/fr/fabien/webcrawler/WebCrawlerApplicationTests.java
@@ -1,16 +1,39 @@
 package fr.fabien.webcrawler;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class WebCrawlerApplicationTests {
 
+	MockMvc mockMvc;
+
+	@Autowired
+	protected WebApplicationContext wac;
+
+	@Autowired
+	OfferController offerController;
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = standaloneSetup(this.offerController).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void testGetOffers() throws Exception {
+		mockMvc.perform(get("/getOffers/adsearch").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
 	}
 
 }
